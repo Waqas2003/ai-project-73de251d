193e219a3c2992ef319a9ef7f1ad80205840f949
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-03 23:51:53 +0500

    Add src\index.js

---
+++ src\index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Flag from './Flag';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Flag />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
